e4f84ee859cf5022462de72441839f5e1f4a5889
Sergio Elisondo <sergioelisondo@gmail.com>
2020-11-09 01:11:08 -0800

    assignment operators in depth

--- assignment operators.js
+++ assignment operators.js
@@ -3,3 +3,9 @@ let y = 4; // y contains the value 4
 x = y; // x now contains the same value y contains, 4
 
 console.log(x)
+
+
+// +=	Addition assignment	Adds the value on the right to the variable value on the left, then returns the new variable value	x += 4;	x = x + 4;
+// -=	Subtraction assignment	Subtracts the value on the right from the variable value on the left, and returns the new variable value	x -= 3;	x = x - 3;
+// *=	Multiplication assignment	Multiplies the variable value on the left by the value on the right, and returns the new variable value	x *= 3;	x = x * 3;
+// /=	Division assignment	Divides the variable value on the left by the value on the right, and returns the new variable value	x /= 5;	x = x / 5;
