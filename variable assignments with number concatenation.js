6d2918e94e743e25709f1efb44db22b7fb276fae
Sergio Elisondo <sergioelisondo@gmail.com>
2020-11-17 22:36:42 -0800

    these are string with variable concatenation while declaring a math problem (then using it) from a variable

---
+++ variable assignments with number concatenation.js
@@ -0,0 +1,7 @@
+let cat1 = 'toshi'
+let cat2 = ' yuki' // add space
+
+let cat3 = cat1 + cat2
+let cat4 = cat3 + ' fats' 
+let addingAge = 5 + 5
+console.log('these are my cats: ' + cat4 + ' that are ' + addingAge + ' years old.' ) // added space in strings
