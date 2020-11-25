0a352f7d689fdbb791ea781c98f2f4069bf9e12a
Sergio Elisondo <sergioelisondo@gmail.com>
2020-11-25 14:34:51 -0800

    adding to variable folder practice

--- consolelog.js
+++ consolelog.js
@@ -51,4 +51,4 @@ let talkingCats2 = 'this is camel case with a number'
 console.log(talkingCats2)
 
 let catsDontTalk = "they meow"
-console.log(catsDontTalk + " adding more to it")
+console.log(catsDontTalk, " adding more to it", (2 + 1))
