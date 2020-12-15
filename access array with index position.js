b2967281074ea6efcd0ef5d7f146d225094ca403
Sergio Elisondo <sergioelisondo@gmail.com>
2020-12-15 12:00:12 -0800

    accessing an array with index position

---
+++ access array with index position.js
@@ -0,0 +1,13 @@
+let fruits = ['Apple', 'Banana']
+
+console.log(fruits.length)
+// 2, since there are two elements in the array at index 0 and 1
+
+
+
+let first = fruits[0]
+// Apple
+let last = fruits[fruits.length - 1]
+// Banana
+
+console.log(first + last)
