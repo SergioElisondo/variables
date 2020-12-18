99d39e0de4be88e6d277be08e48c91eb1b9a9116
Sergio Elisondo <sergioelisondo@gmail.com>
2020-12-18 12:08:32 -0800

    removing an item from the end of an array

---
+++ remove item from an the end of an array.js
@@ -0,0 +1,35 @@
+let fruits = ['Apple', 'Banana']
+
+console.log(fruits.length)
+// 2, since there are two elements in the array at index 0 and 1
+
+
+
+let first = fruits[0]
+// Apple
+let last = fruits[fruits.length - 1]
+// Banana
+
+console.log(first + last)
+
+
+fruits.forEach(function(item, index, array) {
+  console.log(item, index)
+})
+// Apple 0
+// Banana 1
+
+
+
+
+let newLength = fruits.push('Orange')
+// ["Apple", "Banana", "Orange"]
+console.log(newLength)
+
+
+
+
+
+let removeLast = fruits.pop() // remove Orange (from the end)
+// ["Apple", "Banana"]
+console.log(removeLast)
