df8044bb8b848697317fb98d500bead5501b26ad
Sergio Elisondo <sergioelisondo@gmail.com>
2021-01-09 12:43:15 -0800

    this is using the .map() method

---
+++ using a map method.js
@@ -0,0 +1,7 @@
+ const arr = [1, 2, 3, 4, 5, 6];
+
+  // add one to every element
+  const oneAdded = arr.map(num => num + 1);
+  console.log(oneAdded); // output [2, 3, 4, 5, 6, 7]
+
+  console.log(arr); // output: [1, 2, 3, 4, 5, 6]
