f0779f0cc2bbae4e3b54e29f5c6ae3fbaa627dd4
Sergio Elisondo <sergioelisondo@gmail.com>
2021-01-14 12:50:12 -0800

    using the sort method

---
+++ sort method.js
@@ -0,0 +1,12 @@
+// This method used to arrange/sort array’s item either ascending or descending order.
+
+const arr = [1, 2, 3, 4, 5, 6];
+  const alpha = ['e', 'a', 'c', 'u', 'y'];
+
+  // sort in descending order
+  descOrder = arr.sort((a, b) => a > b ? -1 : 1);
+  console.log(descOrder); // output: [6, 5, 4, 3, 2, 1]
+
+  // sort in ascending order
+  ascOrder = alpha.sort((a, b) => a > b ? 1 : -1);
+  console.log(ascOrder); // output: ['a', 'c', 'e', 'u', 'y']
