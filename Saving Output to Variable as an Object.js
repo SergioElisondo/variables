425d815e3e035519fb4f114fc39e9dc36126ac7e
Sergio Elisondo <sergioelisondo@gmail.com>
2021-04-11 22:21:15 -0700

    added to saving output to variable file

--- Saving Output to Variable as an Object.js
+++ Saving Output to Variable as an Object.js
@@ -4,3 +4,15 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
   .then(res => res.json())
   .then(data => obj = data)
   .then(() => console.log(obj))
+
+  // chained promise
+  const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('foo');
+  }, 300);
+});
+
+myPromise
+  .then(handleResolvedA, handleRejectedA)
+  .then(handleResolvedB, handleRejectedB)
+  .then(handleResolvedC, handleRejectedC);
