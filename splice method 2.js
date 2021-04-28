987c21f86d6c3f847701a2003b751182b4c36dc8
Sergio Elisondo <sergioelisondo@gmail.com>
2021-04-27 22:47:27 -0700

    adding more info to splice array method 2

--- splice method 2.js
+++ splice method 2.js
@@ -4,6 +4,10 @@ arr.splice(1, 1); // from index 1 remove 1 element
 
 alert( arr ); // ["I", "JavaScript"]
 
+
+
+
+
 let arr = ["I", "study", "JavaScript", "right", "now"];
 
 // remove 3 first elements and replace them with another
@@ -11,9 +15,27 @@ arr.splice(0, 3, "Let's", "dance");
 
 alert( arr ) // now ["Let's", "dance", "right", "now"]
 
+
+
+
+
 let arr = ["I", "study", "JavaScript", "right", "now"];
 
 // remove 2 first elements
 let removed = arr.splice(0, 2);
 
 alert( removed ); // "I", "study" <-- array of removed elements
+
+
+
+
+
+
+let arr = ["I", "study", "JavaScript"];
+
+// from index 2
+// delete 0
+// then insert "complex" and "language"
+arr.splice(2, 0, "complex", "language");
+
+alert( arr ); // "I", "study", "complex", "language", "JavaScript"
