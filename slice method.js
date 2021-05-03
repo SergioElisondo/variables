9fb5825df147ec2b9ff4cdfb05be03c922d5dc2b
Sergio Elisondo <sergioelisondo@gmail.com>
2021-05-03 14:02:39 -0700

    updating the array slice method file

--- slice method.js
+++ slice method.js
@@ -1,3 +1,12 @@
 let str = "toshi, banana, yuki";
 let res = str.slice(7, 13);
 console.log(res)
+
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+console.log(animals.slice(2));
+// expected output: Array ["camel", "duck", "elephant"]
+console.log(animals.slice(2, 4));
+// expected output: Array ["camel", "duck"]
+console.log(animals.slice(1, 5));
+// expected output: Array ["bison", "camel", "duck", "elephant"]
