67dbe67f23a39121bb620ba4864bffeeb36c951a
Sergio Elisondo <sergioelisondo@gmail.com>
2021-05-06 17:37:27 -0700

    this is an update on the slice method

--- slice method.js
+++ slice method.js
@@ -10,3 +10,12 @@ console.log(animals.slice(2, 4));
 // expected output: Array ["camel", "duck"]
 console.log(animals.slice(1, 5));
 // expected output: Array ["bison", "camel", "duck", "elephant"]
+
+
+
+// Remove 1 element at index 2, and insert "trumpet"
+let myFish = ['angel', 'clown', 'drum', 'sturgeon']
+let removed = myFish.splice(2, 1, 'trumpet')
+
+// myFish is ["angel", "clown", "trumpet", "sturgeon"]
+// removed is ["drum"]
