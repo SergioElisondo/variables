8b545e356022d3a9c07fa287e78c4610218af6ad
Sergio Elisondo <sergioelisondo@gmail.com>
2021-05-13 16:16:37 -0700

    added more to the splice methods

--- splice method 2.js
+++ splice method 2.js
@@ -120,3 +120,15 @@ let removed = myFish.splice(2)
 
 // myFish is ["angel", "clown"]
 // removed is ["mandarin", "sturgeon"]
+
+//example 13
+const months = ['Jan', 'March', 'April', 'June'];
+months.splice(1, 0, 'Feb');
+// inserts at index 1
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "June"]
+
+months.splice(4, 1, 'May');
+// replaces 1 element at index 4
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "May"]
