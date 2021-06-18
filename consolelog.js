1e28107614f69e4a7702fb3d597f5b4939e8007a
Sergio Elisondo <sergioelisondo@gmail.com>
2021-06-18 15:13:26 -0700

    updated file

--- consolelog.js
+++ consolelog.js
@@ -44,3 +44,26 @@ console.log(toshi, toshi)
 let gatos = (yuki, toshi)
 console.log =(gatos, gatos, gatos, yuki, yuki)
 
+let talkingCats = 'this is camel case'
+console.log(talkingCats)
+
+let talkingCats2 = 'this is camel case with a number'
+console.log(talkingCats2)
+
+let catsDontTalk = "they meow"
+console.log(catsDontTalk, " adding more to it", (2 + 1), "this can keep going", "and going", "and going", "now let's add some numbers", 120 + 21)
+
+// making the output connect in an odd way
+let thisWillBeLong = "well, not this part"
+console.log(thisWillBeLong, "as with the above idea", " this can keep going", "while add both", yuki, "and", toshi)
+
+let test = "template string literals"
+console.log(`${test} with some strings`, 24 + 21, "this worked", 302/21)
+
+let anotherTest = "TSL test"
+console.log(`${anotherTest}`, 30 % 3, `would this work? sure does`, "you can use multiple string literals ", `${anotherTest} so let's keep going!`, "be sure to separate with commas", 50%2, "and it can keep going" )
+
+let test3 = "didn't really add functions here"
+console.log('test3', 'still much practice', `using STL`)
+
+// looking back at practicing -- silly notes lol
