1c74157db7971566bbd4b5fa29cd22c11f5a224f
Sergio Elisondo <sergioelisondo@gmail.com>
2021-10-12 09:22:46 -0700

    ParseInt Function with a Radix - change base

---
+++ ParseInt Function with a Radix - change base.js
@@ -0,0 +1,7 @@
+function convertToInteger(str) {
+return parseInt(str, 2)
+// second argument is a radix
+// radix specifies which base number to use, in this case it's binary
+}
+
+convertToInteger("10011");
